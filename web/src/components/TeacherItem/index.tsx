3da4db23b5dd2faa4ddbcee8a2cd8fea5f09d53f
juliastefanoni <juliastefanonidev@gmail.com>
2020-08-09 23:47:30 -0300

    Application frontend finalized :pushpin: Frontend consuming the built API :pushpin: Teacher registration screens, and completed teacher list :pushpin: Cost per class formatting :pushpin: Contact by whatsapp with standard message

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,65 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const message = `Olá ${teacher.name}, 
+    encontrei o seu perfil no Proffy e gostaria de mais detalhes sobre as
+    aulas de ${teacher.subject}`;
+
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/53796153?s=460&u=5fb4f4f34f78ee55148b7a69c269339105d68408&v=4" alt="Júlia Stefanoni"/>
+        <img src={teacher.avatar} alt="Júlia Stefanoni"/>
         <div>
-          <strong> Júlia Stefanoni </strong>
-          <span> Química </span>
+          <strong> {teacher.name} </strong>
+          <span> {teacher.subject} </span>
         </div>
       </header>
     
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br /> <br/>
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões
-      </p>
+      <p> {teacher.bio} </p>
       
       <footer>
         <p>
           Preço/hora
-          <strong> R$ 20,00 </strong>
+          <strong> 
+            {new Intl.NumberFormat('pt-BR', 
+              { 
+                style: 'currency',
+                currency: 'BRL' 
+              }).format(teacher.cost)
+            }
+          </strong>
         </p>
-        <button type="button">
+        <a 
+          href={`https://wa.me/${teacher.whatsapp}?text=${message}`} 
+          onClick={createNewConnection} 
+          target="_blank"
+        >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
     );
